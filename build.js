d85b76beb745f220c65c98be19dd0c131b356806
Alexandru Branza <branza.alex@gmail.com>
2022-02-11 14:57:42 +0200

    Remove Large Deps

--- build.js
+++ build.js
@@ -1,37 +1,56 @@
 const fs = require('fs')
-const path = require('path')
+const needle = require('needle')
 
-const artLocal = './node_modules/stremio-art/'
+const artRepo = 'Stremio/stremio-art'
 
-const allImages = fs.readdirSync(path.join(artLocal, 'originals'))
-const allThumbnails = fs.readdirSync(path.join(artLocal, 'thumbnails'))
+function initBuild(allThumbnails, allImages) {
+	const artRemote = 'https://github.com/'+artRepo+'/raw/main/'
 
-const artRemote = 'https://github.com/Stremio/stremio-art/raw/main/'
+	const galleryTemplate = fs.readFileSync('./html-templates/main.html').toString()
 
-const galleryTemplate = fs.readFileSync('./html-templates/main.html').toString()
+	const imageTemplate = fs.readFileSync('./html-templates/item-image.html').toString()
+	const videoTemplate = fs.readFileSync('./html-templates/item-video.html').toString()
 
-const imageTemplate = fs.readFileSync('./html-templates/item-image.html').toString()
-const videoTemplate = fs.readFileSync('./html-templates/item-video.html').toString()
+	const allItems = allImages.map(item => {
+		const hasThumb = allThumbnails.includes(item)
+		const extension = item.split('.').pop().toLowerCase()
+		const name = item.replace(/\.[^/.]+$/, "").split('-').join(' ')
+		if (['jpg','gif','png'].includes(extension)) {
+			return imageTemplate
+					.split('{item-name}').join(item)
+					.replace('{image-url}', artRemote + (hasThumb ? 'thumbnails' : 'originals') + '/' + encodeURIComponent(item))
+					.replace('{image-name}', name)
+		} else if (['mp4'].includes(extension)) {
+			return videoTemplate
+					.split('{item-name}').join(item)
+					.replace('{video-url}', artRemote + 'originals' + '/' + encodeURIComponent(item))
+					.replace('{video-name}', name)
+		} else {
+			return false
+		}
+	}).filter(el => !!el)
 
-const allItems = allImages.map(item => {
-	const hasThumb = allThumbnails.includes(item)
-	const extension = item.split('.').pop().toLowerCase()
-	const name = item.replace(/\.[^/.]+$/, "").split('-').join(' ')
-	if (['jpg','gif','png'].includes(extension)) {
-		return imageTemplate
-				.split('{item-name}').join(item)
-				.replace('{image-url}', artRemote + (hasThumb ? 'thumbnails' : 'originals') + '/' + encodeURIComponent(item))
-				.replace('{image-name}', name)
-	} else if (['mp4'].includes(extension)) {
-		return videoTemplate
-				.split('{item-name}').join(item)
-				.replace('{video-url}', artRemote + 'originals' + '/' + encodeURIComponent(item))
-				.replace('{video-name}', name)
+	const gallery = galleryTemplate.replace('{art-catalog}', allItems.join(''))
+
+	fs.writeFileSync('./index.html', gallery)
+
+	console.log('Build finished: ./index.html')
+}
+
+needle.get('https://api.github.com/repos/'+artRepo+'/contents/thumbnails', (err, resp, body) => {
+	if (((body || [])[0] || {}).name) {
+		const allThumbnails = body.map(el => el.name).filter(el => !!el)
+		needle.get('https://api.github.com/repos/'+artRepo+'/contents/thumbnails', (err, resp, body) => {
+			if (((body || [])[0] || {}).name) {
+				const allImages = body.map(el => el.name).filter(el => !!el)
+				initBuild(allThumbnails, allImages)
+			} else {
+				console.log('Could not retrieve thumbnails from stremio-art')
+				process.exit(0)
+			}
+		})
 	} else {
-		return false
+		console.log('Could not retrieve thumbnails from stremio-art')
+		process.exit(0)
 	}
-}).filter(el => !!el)
-
-const gallery = galleryTemplate.replace('{art-catalog}', allItems.join(''))
-
-fs.writeFileSync('./index.html', gallery)
+})
